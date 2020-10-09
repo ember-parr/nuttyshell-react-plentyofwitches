128249cee679eb23540484e86d6f2aac48111444
Brady Logan <brady.drew.logan@gmail.com>
2020-10-09 15:29:12 -0500

    added comments

--- src/components/messages/MessageProvider.js
+++ src/components/messages/MessageProvider.js
@@ -1,18 +1,21 @@
 import React, { useState, createContext } from "react"
 
-
+//create the MessageContext
 export const MessageContext = createContext()
 
 export const MessageProvider = props => {
 
+    //set state for messages
     const [ messages, setMessages ] = useState([])
 
+    //get messages from the database
     const getMessages = () => {
         return fetch("http://localhost:8088/messages?_expand=user")
         .then(res => res.json())
         .then(setMessages)
     }
 
+    //context provides messages and get messages
     return (
         <MessageContext.Provider value={{
             messages, getMessages
