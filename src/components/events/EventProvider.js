77fb96ca288d739d52dba11df2a52cdcdf0318c2
Colin Arel <romans_one16@live.com>
2020-10-09 15:57:10 -0500

    pre-pull save...

---
+++ src/components/events/EventProvider.js
@@ -0,0 +1,55 @@
+import React, { useState, createContext } from "react"
+
+export const EventContext = createContext()
+
+export const EventProvider = (props) => {
+    const [events, setEvents] = useState([])
+
+    const getEvents = () => {
+        return fetch("http://localhost:8088/events?_expand=location")
+            .then(res => res.json())
+            .then(setEvents)
+    }
+
+    const addEvent = eventObj => {
+        return fetch("http://localhost:8088/events?_expand=location", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(eventObj)
+        })
+            .then(getEvents)
+    }
+
+    const getEventById = (id) => {
+        return fetch(`http://localhost:8088/events/${id}?_expand=location`)
+            .then(res => res.json())
+    }
+
+    const deleteEvent = eventId => {
+        return fetch(`http://localhost:8088/events/${eventId}`, {
+            method: "DELETE"
+        })
+            .then(getEvents)
+    }
+
+    const updateEvent = event => {
+        return fetch(`http://localhost:8088/events/${event.id}?_expand=location`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(event)
+        })
+            .then(getEvents)
+    }
+
+    return (
+        <EventContext.Provider value={{
+            events, getEvents, addEvent, getEventById, deleteEvent, updateEvent
+        }}>
+            {props.children}
+        </EventContext.Provider>
+    )
+}
