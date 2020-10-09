48a1f3ea1ecec8fb245db92ac44bd435b8dfa1ba
Colin Arel <romans_one16@live.com>
2020-10-09 13:58:19 -0500

    addded index.js and Home.js... testing npm start functionality

--- src/components/Nuttyshell.js
+++ src/components/Nuttyshell.js
@@ -1,1 +1,12 @@
-//This is where we will render our major components
+import React from "react"
+// import { Route } from "react-router-dom"
+// import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./NuttyShell.css"
+
+export const NuttyShell = () => (
+    <>
+        {/* <NavBar /> */}
+        <ApplicationViews />
+    </>
+)
