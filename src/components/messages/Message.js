1033147397a7484b2e78b6d9f35c40829c21d5a3
Brady Logan <brady.drew.logan@gmail.com>
2020-10-12 14:56:47 -0500

    changed Message

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -6,14 +6,16 @@ export const Message = ({message}) => {
 
     const userId = parseInt(localStorage.getItem("user"))
 
+
     return (
         <ListGroupItem className="message-container">
             <InputGroup>
                 <InputGroupText className="message_text">{message.user.username}: {message.message}</InputGroupText>
-                {{if(parseInt(message.userId) === userId){
-
-                }}}
-                <InputGroupAddon addonType="append"><Button color="danger">X</Button></InputGroupAddon>
+                {{if(parseInt(message.userId) === userId)}}{
+                    <InputGroupAddon addonType="append"><Button color="danger">X</Button></InputGroupAddon>
+                }  
+                
+                
             </InputGroup>
         </ListGroupItem>
     )
