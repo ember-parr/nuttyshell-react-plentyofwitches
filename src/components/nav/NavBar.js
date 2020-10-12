465f0688bdf666a1500f142368aa79aa26c484f6
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-12 14:35:32 -0400

    added sticky header

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,27 +3,25 @@ import { Nav, NavItem, NavLink } from "reactstrap";
 
 export const NavBar = (props) => {
   return (
-    <div>
-      <Nav>
-        <NavItem>
-          <NavLink href="/">Home</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/messages">Messages</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/events">Events</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/tasks">Tasks</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/articles">Articles</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/friends">Friends</NavLink>
-        </NavItem>
-      </Nav>
-    </div>
+    <Nav className="fixed-sticky">
+      <NavItem>
+        <NavLink href="/">Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/messages">Messages</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/events">Events</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/tasks">Tasks</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/articles">Articles</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/friends">Friends</NavLink>
+      </NavItem>
+    </Nav>
   );
 };
