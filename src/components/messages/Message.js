0b12cc173f6d54ce548b059a59e1a82ffb10c593
Brady Logan <brady.drew.logan@gmail.com>
2020-10-12 12:13:06 -0500

    created removeMessage

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -1,8 +1,11 @@
 import React from "react"
-import { ListGroupItem } from 'reactstrap'
+import { ListGroupItem, Card, CardBody, CardText, CardGroup, Button } from 'reactstrap'
 
 //create HTML for a single message
 export const Message = ({message}) => {
+
+    const userId = parseInt(localStorage.getItem("user"))
+
     return (
         <ListGroupItem className="message-container">
             <div className="message_user">{message.user.username}:</div>
