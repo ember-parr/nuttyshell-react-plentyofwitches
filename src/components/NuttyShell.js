18c13f667114bc59a921a8afdea0098703e92247
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-12 15:14:27 -0400

    change file name

--- src/components/NuttyShell.js
+++ src/components/NuttyShell.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { NavBar } from "./nav/NavBar";
+import { NavBarComponent } from "./nav/NavBarComponent";
 import { ApplicationViews } from "./ApplicationViews";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
 
 export const NuttyShell = () => (
   <>
-    <NavBar />
+    {/* <NavBarComponent /> */}
     <ApplicationViews />
     <Route path="/login">
       <Login />
