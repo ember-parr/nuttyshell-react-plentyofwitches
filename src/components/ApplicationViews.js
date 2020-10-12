e8c97b6be886cc225928eb25debfe9fe03055d83
Ember Parr <ember-devops@gmail.com>
2020-10-12 14:41:23 -0500

    fixes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./home/Home"
+import { ArticleProvider } from "./articles/ArticleProvider"
+import { ArticleList } from "./articles/ArticleList"
+import { ArticleDetail } from "./articles/ArticleDetail"
+import { ArticleForm } from "./articles/ArticleForm"
 
 export const ApplicationViews = (props) => {
     return (
@@ -9,6 +13,30 @@ export const ApplicationViews = (props) => {
             <Route exact path="/">
                 <Home />
             </Route>
+
+            <ArticleProvider>
+                <Route exact path="/articles">
+                    <ArticleList />
+                </Route>
+            </ArticleProvider>
+
+            <ArticleProvider>
+               <Route exact path="/articles/detail/:articleId(\d+)">
+                    <ArticleDetail />
+                </Route>
+            </ArticleProvider> 
+
+            <ArticleProvider>
+                <Route exact path="/articles/create">
+                    <ArticleForm />
+                </Route>
+            </ArticleProvider>
+
+            <ArticleProvider>
+                <Route exact path="/articles/edit/:articleId(\d+)">
+                    <ArticleForm />
+                </Route>
+            </ArticleProvider>
         </>
     )
 }
