9fb68660da15a8960ac34900d7e8b82cd68c6ec4
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-13 11:43:22 -0400

    added footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Nav } from "reactstrap";
+
+export const Footer = (props) => {
+  return (
+    <div className="sticky-bottom bg-primary text-white p-3 text-center mt-5">
+      Plenty of Witches &#169; 2020
+    </div>
+  );
+};
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { Nav, NavItem, NavLink } from "reactstrap";
+import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
 
-export const NavBarComponent = (props) => {
+export const NavBar = (props) => {
   return (
-    <Nav>
-      <NavItem>
-        <NavLink href="/">Home</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/events">Events</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/tasks">Tasks</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/articles">Articles</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/friends">Friends</NavLink>
-      </NavItem>
-    </Nav>
+    <Navbar className="fixed-top" color="primary" light expand="md">
+      <Nav>
+        <NavItem>
+          <NavLink className="text-white" href="/" active>
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem className="justify-content-end">
+          <NavLink className="text-white" href="/events">
+            Events
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="text-white" href="/tasks">
+            Tasks
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="text-white" href="/articles">
+            Articles
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="text-white" href="/friends">
+            Friends
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 };
