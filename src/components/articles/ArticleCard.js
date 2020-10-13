55d297987ecaf9dce780aa0af296e9f9037efc76
Ember Parr <ember-devops@gmail.com>
2020-10-13 15:50:48 -0500

    added author on article card

--- src/components/articles/ArticleCard.js
+++ src/components/articles/ArticleCard.js
@@ -1,17 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 import { useHistory } from "react-router-dom"
-import { Card, Button, CardHeader, CardText, Col, Tooltip, tooltipOpen, toggle } from 'reactstrap';
+import { Card, Button, CardHeader, CardText, Col, Tooltip } from 'reactstrap';
 
 
 
 export const ArticleCard = ({ article }) => {
     const domHistory = useHistory()
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+    const toggle = () => setTooltipOpen(!tooltipOpen);
     return (
       
         <Col sm="12">
           <Card body>
-            <CardHeader>{ article.newsTitle }</CardHeader>
+          <CardHeader>{ article.newsTitle }</CardHeader>
             <CardText>{ article.newsContent }</CardText>
+            <CardText>Added by: { article.user.firstName} </CardText>
             <Button onClick={() => domHistory.push(`/articles/detail/${article.id}`)}>Details</Button>
           </Card>
         </Col>
